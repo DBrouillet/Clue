e04031d64cbe21530cbcbaa13462907126690c29
Miika Jarvela <mjarvela@mines.edu>
2019-02-26 11:56:48 -0700

    Created stubs for BoardCell and IntBoard classes

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -1,5 +1,12 @@
 package experiment;
 
 public class BoardCell {
-
+	private int row;
+	private int column;
+	
+	public BoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,32 @@
 package experiment;
 
-public class IntBoard {
+import java.util.Map;
+import java.util.Set;
 
+public class IntBoard {
+	private BoardCell[][] grid;
+	private Map<BoardCell, Set<BoardCell>> adjMtx;
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
+	
+	public IntBoard() {
+		super();
+		calcAdjacencies();
+	}
+
+	public void calcAdjacencies() {
+		
+	}
+	
+	public Set<BoardCell> getAdjList(BoardCell cell) {
+		return adjMtx.get(cell);
+	}
+	
+	public void calcTargets(BoardCell startCell, int pathLength) {
+		
+	}
+	
+	public Set<BoardCell> getTargets() {
+		return targets;
+	}
 }
