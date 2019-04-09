8ae016595a3c5276f5642f08d432c6819df14c00
RichardFigueroaErickson <rfigueroaerickson@mymail.mines.edu>
2019-04-09 17:43:10 -0600

    Added comments.

--- src/clueGame/DetectiveNotes.java
+++ src/clueGame/DetectiveNotes.java
@@ -10,6 +10,11 @@ import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
+/**
+ * @author Miika Jarvela and Daniel Brouillet
+ * Class used to create and display the detective
+ * notes custom dialog to facilitate player note-taking.
+ */
 public class DetectiveNotes extends JDialog {
 	public DetectiveNotes() {
 		Board board = Board.getInstance();
@@ -17,6 +22,8 @@ public class DetectiveNotes extends JDialog {
 		setSize(600, 800);
 		setLayout(new GridLayout(3,2));
 		
+		
+		// Block of code to create and add the People checkboxes.
 		JPanel peopleBoxes = new JPanel();
 		peopleBoxes.setName("People");
 		int numBoxesPerRow = (int) Math.ceil(board.getPlayers().size() / 2.0);
@@ -28,6 +35,7 @@ public class DetectiveNotes extends JDialog {
 		peopleBoxes.setBorder(new TitledBorder (new EtchedBorder(), "People"));
 		add(peopleBoxes);
 		
+		// Block of code to create and add the Person Guess combo box.
 		JPanel playerGuess = new JPanel();
 		Vector<String> players = new Vector<String>();
 		players.add("None");
@@ -39,6 +47,7 @@ public class DetectiveNotes extends JDialog {
 		playerGuess.setBorder(new TitledBorder (new EtchedBorder(), "Person Guess"));
 		add(playerGuess);
 		
+		// Block of code to create and add the Rooms checkboxes.
 		JPanel placesBoxes = new JPanel();
 		placesBoxes.setName("Place");
 		numBoxesPerRow = (int) Math.ceil(board.getRoomNames().size() / 2.0); 
@@ -50,6 +59,7 @@ public class DetectiveNotes extends JDialog {
 		placesBoxes.setBorder(new TitledBorder (new EtchedBorder(), "Rooms"));
 		add(placesBoxes);
 		
+		// Block of code to create and add the Rooms combo box.
 		JPanel placesGuess = new JPanel();
 		Vector<String> places = new Vector<String>();
 		places.add("None");
@@ -61,6 +71,7 @@ public class DetectiveNotes extends JDialog {
 		placesGuess.setBorder(new TitledBorder (new EtchedBorder(), "Room Guess"));
 		add(placesGuess);
 		
+		// Block of code to create and add the Weapons checkboxes.
 		JPanel weaponBoxes = new JPanel();
 		weaponBoxes.setName("Weapons");
 		numBoxesPerRow = (int) Math.ceil(board.getWeapons().size() / 2.0); 
@@ -72,6 +83,7 @@ public class DetectiveNotes extends JDialog {
 		weaponBoxes.setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
 		add(weaponBoxes);
 		
+		// Block of code to create and add the Weapons combo box.
 		JPanel weaponsGuess = new JPanel();
 		Vector<String> weapons = new Vector<String>();
 		weapons.add("None");
